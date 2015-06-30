70f20a2cbe8797d6ac58934168abd441bf76dac3
mrwutong <mrwutong@163.com>
2015-06-30 15:59:33 +0800

    微信支付IOS版更新

--- src/android/Constants.java
+++ src/android/Constants.java
@@ -9,8 +9,6 @@ package com.qdc.plugins.weixin;
 public class Constants {
 
 	// appid
-	// 请同时修改 androidmanifest.xml里面，.PayActivityd里的属性<data
-	// android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
 	public static final String APP_ID = "wxf2f565574a968187";
 
 	// 商户号
--- www/wxpay.js
+++ www/wxpay.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 var wxpay = {
-  payment: function(args, successFn, failureFn) {
-    exec(successFn, failureFn, 'WeixinPay', 'payment', args);
+  payment: function(json, successFn, failureFn) {
+    exec(successFn, failureFn, 'WeixinPay', 'payment', [json]);
   }
 }
 
